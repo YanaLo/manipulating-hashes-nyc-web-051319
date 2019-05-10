3f3ee411b4c21c567990bbbef76a1bbed121c0d4
yana.loginova <yana.loginova@cix.csi.cuny.edu>
2019-05-10 09:28:09 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -20,6 +20,6 @@ def first_challenge
   contacts.each do |person, info|
     info.each do |k,v|
       if k==:favorite_icecream_flavors
-        v.delete_if
+        v.delete_if{|flavor| flavor="strawberry"
 end
 
